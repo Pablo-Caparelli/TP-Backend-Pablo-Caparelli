adcd88023d5d782ba0338d4c5bf6f9f843e54e04
pablo-caparelli <pablocaparelli@gmail.com>
2025-05-07 17:13:54 -0300

    incorporando la conexión de mongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Probando...");
+import { Schema, model, connect } from "mongoose";
+
+process.loadEnvFile();
+
+const URI_DB = process.env.URI_DB || "";
+
+const connectMongoDb = async () => {
+  try {
+    await connect(URI_DB);
+    console.log("Conectado a MongoDB");
+  } catch (error) {
+    console.log("Error al conectarse a MongoDB:");
+  }
+};
+
+connectMongoDb();
